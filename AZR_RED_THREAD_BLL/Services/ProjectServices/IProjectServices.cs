9c84948eed7ffb7eed5ba52eb4d1fad7ee537bb0
Ssh_Chris <chrisppoyoumbissi39@gmail.com>
2025-09-19 12:30:14 +0100

    feat: DTO pour la création de projet

---
+++ AZR_RED_THREAD_BLL/Services/ProjectServices/IProjectServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZR_RED_THREAD_BLL.Services.ProjectServices
+{
+    internal class IProjectServices
+    {
+    }
+}
